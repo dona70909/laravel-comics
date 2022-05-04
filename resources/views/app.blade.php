2f39ccaafec2c3f2b260955e339d0a548dda5ecb
Donatella7 <campaniellodonatella@gmail.com>
2022-05-04 19:21:56 +0200

    char page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- css --}}
     <link rel="stylesheet" href="{{asset("css/app.css")}}"> 
-    <title>Document</title>
+    <title>Dc Comics</title>
 </head>
 <body>
     @include('header.header')
