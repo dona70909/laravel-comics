2ec65da4babaca6bd897f24aea34bbc30daf3655
Donatella7 <campaniellodonatella@gmail.com>
2022-05-04 14:15:51 +0200

    logo header

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,7 +1,7 @@
 
 <header class="my-header-content d-flex padding">
     <div class="my-header-logo">
-        <img :src="require('@/assets/img/dc-logo.png')" alt="logo">
+        <img src="{{asset('img/dc-logo.png')}}" alt="logo">
     </div>
     <div class="my-header-nav">
         <ul class="d-flex my-link-list" >
