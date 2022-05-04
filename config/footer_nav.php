372b525234c695b99c796b571114d779fb08a552
Donatella7 <campaniellodonatella@gmail.com>
2022-05-04 18:02:16 +0200

    nav links

---
+++ config/footer_nav.php
@@ -0,0 +1,43 @@
+<?php
+
+    return [
+        "linkFooterComics" => [
+
+            ["text" => "Characters", "url" => "#"],
+            ["text" => "Comics","url" => "#"],
+            ["text" => "Movies","url" => "#"],
+            ["text" => "Tv", "url" => "#"],
+            ["text" => "Games","url" => "#"],
+            ["text" => "Videos","url" => "#"],
+    
+        ],
+
+        "linkFooterDc" => [
+            ["text" => "Characters", "url" => "#"],
+            ["text" => "Comics","url" => "#"],
+            ["text" => "Movies","url" => "#"],
+            ["text" => "Tv", "url" => "#"],
+            ["text" => "Games","url" => "#"],
+            ["text" => "Videos","url" => "#"],
+            ["text" => "News", "url" => "#"],
+            ["text" => "Games", "url" => "#"],
+            ["text" => "Videos","url" => "#"],
+            ["text" => "News", "url" => "#"],
+        ],
+
+        "linkFooterSites" => [
+            ["text" => "Characters", "url" => "#"],
+            ["text" => "Comics", "url" => "#"],
+            ["text" => "Movies", "url" => "#"],
+            ["text" => "Tv", "url" => "#"],
+            ["text" => "Tv", "url" => "#"],
+        ],
+
+        "linkFooterShop" => [
+            ["text" => "Characters", "url" => "#"],
+            ["text "=> "Comics", "url" => "#"],
+        ],  
+
+    ]
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,7 @@ Route::get('/', function () {
     $comics_cards = config("comics");
     
     $main_logos_dc = config("logos_dc");
-    return view('app', ["links" => $nav_links,"comics" => $comics_cards,"logos_dc_main" => $main_logos_dc]);
+
+    $footer_nav_links = config("footer_nav");
+    return view('app', ["links" => $nav_links,"comics" => $comics_cards,"logos_dc_main" => $main_logos_dc, "nav_links" => $footer_nav_links ]);
 });
