6a278a2feb8bb778a350c9156fccc54bb3896936
Donatella7 <campaniellodonatella@gmail.com>
2022-05-05 18:50:52 +0200

    ad img

--- resources/views/guest/card.blade.php
+++ resources/views/guest/card.blade.php
@@ -4,11 +4,23 @@
     @include('partials.main.partials.jumbo') 
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-10">
-                <div class="col-7">
-                    <h3>{{$comic["description"]}}</h3>
+            
+            <div class="col-7 card-description-box">
+                <h3>{{$comic["title"]}}</h3>
+                <div class="banner-price d-flex justify-content-between">
+                    <h4>U.S. Price <span class="price-tag">{{$comic["price"]}}</span><h4>
+                    <div class="available-box">
+                        <h3 class="text-transform-uppercase d-inline">Available</h3>
+                        <h3 class="box-check-available d-inline">Check Availability</h3>
+                    </div>    
                 </div>
+                <p>{{$comic["description"]}}</p>
             </div>
+
+            <div class="col-3">
+                <img src="{{asset('img/adv.jpg')}}" alt="ad" class="img-fluid">
+            </div>
+            
         </div>
     </div>
 @endsection
