82a794d35332e023f276655da477f8a1136c64d1
Donatella7 <campaniellodonatella@gmail.com>
2022-05-05 19:42:43 +0200

    writer

--- resources/views/guest/card.blade.php
+++ resources/views/guest/card.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
     @include('partials.main.partials.jumbo') 
     <div class="container-fluid">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center p-4">
             
             <div class="col-7 card-description-box">
                 <h3>{{$comic["title"]}}</h3>
@@ -19,6 +19,34 @@
 
             <div class="col-3">
                 <img src="{{asset('img/adv.jpg')}}" alt="ad" class="img-fluid">
+            </div>
+            
+        </div>
+
+        <div class="row talent-box justify-content-center p-4">
+            <div class="col-5 ">
+                <h3>Talent</h3>
+                <div class="d-flex talent-box-art">
+                    <h4 class="me-2 flex-none">Art by:</h4>
+                    <p>
+                        @foreach($comic["artists"] as $artist)
+                            {{$artist.", "}} 
+                        @endforeach
+                    </p> 
+                </div>
+
+                <div class="d-flex talent-box-written">
+                    <h4 class="me-2 flex-none">Written by:</h4>
+                    <p>
+                        @foreach($comic["writers"] as $writer)
+                            {{$writer.", "}} 
+                        @endforeach
+                    </p> 
+                </div>
+            </div>
+
+            <div class="col-5">
+                <h3>Spec</h3>
             </div>
             
         </div>
